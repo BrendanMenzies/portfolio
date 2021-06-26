f6cc218ba34f53f140f26b04d1ec4fdbc07bb41f
Ubuntu <bmenzies@our.ecu.edu.au>
2021-06-26 06:25:07 +0000

    week6 final

--- week6/BruteForce.py
+++ week6/BruteForce.py
@@ -6,9 +6,16 @@ import hashlib
        
 passwordHash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824" 
 
-
+#This is the list of characters that will be looped thtough
+#Left out special characters and upper case so it doesnt take as long.
 characters = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
 
+
+#This loops through each element in the character array in 8 different loops
+#this creates words of up to 8 characters - obviously not the most ideal solution
+#I have commented out printing each hash as this really slows it down
+#I have also added a calculating message every 4th character change
+#This is to give the user some feedback that its doing something.
 for a in characters:
     for b in characters:
         for c in characters:
